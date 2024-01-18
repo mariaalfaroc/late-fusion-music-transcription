5f0688683b6d4dc8d8269cbb1faae51b8d26a425
mariaalfaroc <mac77@alu.ua.es>
2024-01-19 00:41:57 +0100

    clean kaldi preprocessing

--- my_utils/kaldi_preprocessing.py
+++ my_utils/kaldi_preprocessing.py
@@ -1,35 +1,31 @@
-# -*- coding: utf-8 -*-
-
-import shutil, pathlib
+import shutil
+import pathlib
 
 import numpy as np
 
-from my_utils.data_processing import load_dictionaries
+from scenarios.folds_creation import load_dictionaries
 
 
-def kaldi_fold(fold_filepath, kaldi_dir, fold_type):
+def write_kaldi_fold(fold_filepath, kaldi_dir, fold_type):
     dst = kaldi_dir / f"ID_{fold_type}.lst"
     shutil.copyfile(src=fold_filepath, dst=dst)
-    return
 
 
-def kaldi_vocabulary(w2i_filepath, kaldi_dir):
+def write_kaldi_vocabulary(w2i_filepath, kaldi_dir):
     w2i, _ = load_dictionaries(w2i_filepath)
     with open(kaldi_dir / "chars.lst", "w") as f:
         for w in w2i.keys():
             f.write(f"{w}\n")
-    return
 
 
-def kaldi_groundtruth(kaldi_dir, id_labels, labels):
+def write_kaldi_groundtruth(kaldi_dir, id_labels, labels):
     with open(kaldi_dir / "grnTruth.dat", "w") as f:
         for id, y_true in zip(id_labels, labels):
             id = pathlib.Path(id)
             f.write(" ".join([str(id.stem)] + y_true) + "\n")
-    return
 
 
-def kaldi_confmat(kaldi_dir, fold_type, id_preds, preds, preds_len):
+def write_kaldi_confmat(kaldi_dir, fold_type, id_preds, preds, preds_len):
     with open(kaldi_dir / f"confMat-{fold_type}", "w") as f:
         for id, y_pred, len in zip(id_preds, preds, preds_len):
             id = pathlib.Path(id)
@@ -37,12 +33,11 @@ def kaldi_confmat(kaldi_dir, fold_type, id_preds, preds, preds_len):
             for ts in np.log(y_pred[:len]):
                 f.write(" ".join([str(c) for c in ts]) + "\n")
             f.write("]\n")
-    return
 
 
+# This function can also be used to parse the results of the Kaldi-combination of word graphs
 def parse_kaldi_groundtruth(filepath):
-    # This function can also be used to parse the results of the Kaldi-combination of word graphs
-    gt = dict()
+    gt = {}
     lines = open(filepath).readlines()
     for line in lines:
         data = line.split()
