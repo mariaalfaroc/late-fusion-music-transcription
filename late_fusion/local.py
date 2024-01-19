df7fb85ff569ac5ee3d69494e0bd0bced477a87f
mariaalfaroc <mac77@alu.ua.es>
2024-01-19 18:22:24 +0100

    clean format

--- late_fusion/local.py
+++ late_fusion/local.py
@@ -8,28 +8,29 @@ import pandas as pd
 import swalign
 
 import config
-from evaluation import compute_metrics
-from kaldi_preprocessing import parse_kaldi_groundtruth
+from networks.test import compute_metrics
+from my_utils.kaldi_preprocessing import parse_kaldi_groundtruth
 
 # Multimodal image and audio music transcription
-# Carlos de la Fuente, Jose J. Valero-Mas, Francisco J. Castellanos, and Jorge Calvo-Zaragoza 
+# Carlos de la Fuente, Jose J. Valero-Mas, Francisco J. Castellanos, and Jorge Calvo-Zaragoza
 
 # OMR and AMT combination at prediction level:
 # 1. Input the score image to the OMR model and the CQT image to the AMT model
 # 2. Obtain predictions for each model
 # 3. Apply alignment policy
-    # 3.1. Greedy decoding policy
-    # 3.2. Merge consecutive symbols (average)
-    # 3.3. Remove CTC-blank symbols
-    # 3.4. Smith-Waterman local alignment algorithm (swalign)
-    # 3.5. Fusion policy
-        # - Both sequences match on a token -> included
-        # - Sequences disagree on a token -> include that of the highest probability
-        # - A sequence misses a token -> include that of the other
+# 3.1. Greedy decoding policy
+# 3.2. Merge consecutive symbols (average)
+# 3.3. Remove CTC-blank symbols
+# 3.4. Smith-Waterman local alignment algorithm (swalign)
+# 3.5. Fusion policy
+# - Both sequences match on a token -> included
+# - Sequences disagree on a token -> include that of the highest probability
+# - A sequence misses a token -> include that of the other
 
 # Steps 3.1 - 3.3 are performed using Kaldi
 
 # --------------------
+
 
 # Utility function for obtaining the predictions and their associated probability arrays out of a list of word graphs files
 def get_predictions_and_probabilities(wg_files):
@@ -45,17 +46,74 @@ def get_predictions_and_probabilities(wg_files):
             if len(tokens) == 2:
                 pred.append(tokens[0])
                 prob.append(float(tokens[-1]))
-        # Patch for ground-truth data test partition of Fold1 
+        # Patch for ground-truth data test partition of Fold1
         if id == "201009318-1,48_2":
             id = "201009318-1_48_2"
         y_pred_acc[id] = pred
         y_pred_prob_acc[id] = prob
     return y_pred_acc, y_pred_prob_acc
 
+
 # --------------------
 
-swalign_vocab = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-"$", "%", "&", "/", "(", ")", "=", "?", "¿", "*", "<", ">", "+", "#", "{", "}", ";", ":", "^", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+swalign_vocab = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "$",
+    "%",
+    "&",
+    "/",
+    "(",
+    ")",
+    "=",
+    "?",
+    "¿",
+    "*",
+    "<",
+    ">",
+    "+",
+    "#",
+    "{",
+    "}",
+    ";",
+    ":",
+    "^",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+]
+
 
 # Utility function for converting a string sequence to a compatible swalign string
 def swalign_preprocess(r, q):
@@ -68,54 +126,62 @@ def swalign_preprocess(r, q):
     q = ["¡"] + [w2swa[i] for i in q] + ["!"]
     return "".join(r), "".join(q), swa2w
 
+
 # This is a modified version of the original dump() method for the Alignment class of the swalign library
 # We have modified it to obtain (in the following order) the query, the matches, and the reference sequences; all of them have the same length
-# Matches is a string that contains either "|" if sequences match on a token, or "." if they disagree, 
+# Matches is a string that contains either "|" if sequences match on a token, or "." if they disagree,
 # or " " if one of them misses a token (in this case the token "-" is included at such position in the corresponding sequence)
 def dump(alignment) -> Tuple[str, str, str]:
     i = alignment.r_pos
     j = alignment.q_pos
 
-    q = ''
-    m = ''
-    r = ''
+    q = ""
+    m = ""
+    r = ""
     qlen = 0
     rlen = 0
 
     for count, op in alignment.cigar:
-        if op == 'M':
+        if op == "M":
             qlen += count
             rlen += count
             for k in range(count):
                 q += alignment.orig_query[j]
                 r += alignment.orig_ref[i]
-                if alignment.query[j] == alignment.ref[i] or (alignment.wildcard and (alignment.query[j] in alignment.wildcard or alignment.ref[i] in alignment.wildcard)):
-                    m += '|'
+                if alignment.query[j] == alignment.ref[i] or (
+                    alignment.wildcard
+                    and (
+                        alignment.query[j] in alignment.wildcard
+                        or alignment.ref[i] in alignment.wildcard
+                    )
+                ):
+                    m += "|"
                 else:
-                    m += '.'
+                    m += "."
                 i += 1
                 j += 1
-        elif op == 'D':
+        elif op == "D":
             rlen += count
             for k in range(count):
-                q += '-'
+                q += "-"
                 r += alignment.orig_ref[i]
-                m += ' '
+                m += " "
                 i += 1
-        elif op == 'I':
+        elif op == "I":
             qlen += count
             for k in range(count):
                 q += alignment.orig_query[j]
-                r += '-'
-                m += ' '
+                r += "-"
+                m += " "
                 j += 1
-        elif op == 'N':
-            q += '-//-'
-            r += '-//-'
-            m += '    '
+        elif op == "N":
+            q += "-//-"
+            r += "-//-"
+            m += "    "
 
     while q and r and m:
         return q, m, r
+
 
 # Utility function for adapting the probility sequence after the swalign computation to be able to obtain the final alignment
 def preprocess_prob(s: str, prob: list):
@@ -129,6 +195,7 @@ def preprocess_prob(s: str, prob: list):
             new_prob.insert(id + count, 0)
             count += 1
     return new_prob
+
 
 # Utility function for obtaining the final alignment sequence based on the fixed fusion policy
 def get_alignment(q: str, m: str, r: str, q_prob: list, r_prob: list) -> str:
@@ -152,26 +219,40 @@ def get_alignment(q: str, m: str, r: str, q_prob: list, r_prob: list) -> str:
             alignment += qv if rv == "-" else rv
     return alignment
 
+
 # Utility function for undoing the swalign preprocess
 def undo_swalign_preprocess(alignment: str, swa2w: dict):
     return [swa2w[i] for i in list(alignment) if i not in ("¡", "!")]
 
+
 # --------------------
 
+
 # Utility function for evaluating a multimodal transcription at prediction level approach over a dataset
-def evaluate_multimodal_transcription(omr_wg_files, amt_wg_files, y_true_acc_dict, match=2, mismatch=-1, gap_penalty=-1):
+def evaluate_multimodal_transcription(
+    omr_wg_files, amt_wg_files, y_true_acc_dict, match=2, mismatch=-1, gap_penalty=-1
+):
     y_true_acc = []
     y_pred_comb_acc = []
     # Obtain predictions and their corresponding probability arrays for OMR task
-    omr_y_pred_acc, omr_y_pred_prob_acc = get_predictions_and_probabilities(omr_wg_files)
+    omr_y_pred_acc, omr_y_pred_prob_acc = get_predictions_and_probabilities(
+        omr_wg_files
+    )
     # Obtain predictions and their corresponding probability arrays for AMT task
-    amt_y_pred_acc, amt_y_pred_prob_acc = get_predictions_and_probabilities(amt_wg_files)
+    amt_y_pred_acc, amt_y_pred_prob_acc = get_predictions_and_probabilities(
+        amt_wg_files
+    )
     # Obtain the callable object of swalign library that contains the align() method that performs the alignment
     scoring = swalign.NucleotideScoringMatrix(match, mismatch)
     # Gap penalty designates scores for insertion or deletion
-    sw =  swalign.LocalAlignment(scoring, gap_penalty)
+    sw = swalign.LocalAlignment(scoring, gap_penalty)
     # Perform the multimodal combination at prediction level
-    for (id_r, r), (id_r_prob, r_prob), (id_q, q), (id_q_prob, q_prob) in zip(omr_y_pred_acc.items(), omr_y_pred_prob_acc.items(), amt_y_pred_acc.items(), amt_y_pred_prob_acc.items()):
+    for (id_r, r), (id_r_prob, r_prob), (id_q, q), (id_q_prob, q_prob) in zip(
+        omr_y_pred_acc.items(),
+        omr_y_pred_prob_acc.items(),
+        amt_y_pred_acc.items(),
+        amt_y_pred_prob_acc.items(),
+    ):
         assert id_r == id_r_prob == id_q == id_q_prob
         # Obtain true label
         y_true_acc.append(y_true_acc_dict[id_r])
@@ -188,10 +269,14 @@ def evaluate_multimodal_transcription(omr_wg_files, amt_wg_files, y_true_acc_dic
         y_pred_comb_acc.append(undo_swalign_preprocess(alignment, swa2w))
     # Compute metrics
     symer, seqer = compute_metrics(y_true_acc, y_pred_comb_acc)
-    print(f"SymER (%): {symer:.2f}, SeqER (%): {seqer:.2f} - From {len(y_true_acc)} samples")
+    print(
+        f"SymER (%): {symer:.2f}, SeqER (%): {seqer:.2f} - From {len(y_true_acc)} samples"
+    )
     return symer, seqer
 
+
 # --------------------
+
 
 # Utility function for performing a k-fold cross-validation multimodal experiment on a single dataset
 def k_fold_multimodal_experiment(match=[2], mismatch=[-1], gap_penalty=[-1]):
@@ -199,13 +284,17 @@ def k_fold_multimodal_experiment(match=[2], mismatch=[-1], gap_penalty=[-1]):
 
     # ---------- PRINT EXPERIMENT DETAILS
 
-    print("k-fold multimodal image and audio music transcription combining kaldi word graphs following an Smith-Waterman policy experiment")
+    print(
+        "k-fold multimodal image and audio music transcription combining kaldi word graphs following an Smith-Waterman policy experiment"
+    )
     print(f"Data used {config.base_dir.stem}")
     print(f"match={match}, mismatch={mismatch}, gap_penalty={gap_penalty}")
 
     # ---------- K-FOLD EVALUATION
 
-    assert os.listdir(config.output_dir / "omr") == os.listdir(config.output_dir / "amt")
+    assert os.listdir(config.output_dir / "omr") == os.listdir(
+        config.output_dir / "amt"
+    )
 
     # Start the k-fold evaluation scheme
     k = len(os.listdir(config.output_dir / "omr"))
@@ -215,13 +304,25 @@ def k_fold_multimodal_experiment(match=[2], mismatch=[-1], gap_penalty=[-1]):
         print(f"Fold {i}")
 
         # Get the current fold data
-        omr_wg_fnames = sorted([os.path.join(config.output_dir / "omr" / f"Fold{i}" / "SW", fname)  for fname in os.listdir(config.output_dir / "omr" / f"Fold{i}" / "SW")])
-        amt_wg_fnames = sorted([os.path.join(config.output_dir / "amt" / f"Fold{i}" / "SW", fname)  for fname in os.listdir(config.output_dir / "amt" / f"Fold{i}" / "SW")])
+        omr_wg_fnames = sorted(
+            [
+                os.path.join(config.output_dir / "omr" / f"Fold{i}" / "SW", fname)
+                for fname in os.listdir(config.output_dir / "omr" / f"Fold{i}" / "SW")
+            ]
+        )
+        amt_wg_fnames = sorted(
+            [
+                os.path.join(config.output_dir / "amt" / f"Fold{i}" / "SW", fname)
+                for fname in os.listdir(config.output_dir / "amt" / f"Fold{i}" / "SW")
+            ]
+        )
         assert len(omr_wg_fnames) == len(amt_wg_fnames)
 
         # Load the current ground-truth data
         # Same file for both models (ofc), so load one of them
-        kaldi_gt_path = config.output_dir / "omr" / f"Fold{i}" / "kaldi" / "grnTruth.dat"
+        kaldi_gt_path = (
+            config.output_dir / "omr" / f"Fold{i}" / "kaldi" / "grnTruth.dat"
+        )
         gt = parse_kaldi_groundtruth(filepath=kaldi_gt_path)
 
         # Set filepaths outputs
@@ -235,8 +336,13 @@ def k_fold_multimodal_experiment(match=[2], mismatch=[-1], gap_penalty=[-1]):
         for m, mism, gp in zip(match, mismatch, gap_penalty):
             # Multimodal transcription evaluation
             symer, seqer = evaluate_multimodal_transcription(
-                omr_wg_files=omr_wg_fnames, amt_wg_files=amt_wg_fnames, y_true_acc_dict=gt,
-                match=m, mismatch=mism, gap_penalty=gp)
+                omr_wg_files=omr_wg_fnames,
+                amt_wg_files=amt_wg_fnames,
+                y_true_acc_dict=gt,
+                match=m,
+                mismatch=mism,
+                gap_penalty=gp,
+            )
             symer_acc.append(symer)
             seqer_acc.append(seqer)
         # Save fold logs
@@ -245,7 +351,7 @@ def k_fold_multimodal_experiment(match=[2], mismatch=[-1], gap_penalty=[-1]):
             "mismatch": mismatch,
             "gap_penalty": gap_penalty,
             "symer": symer_acc,
-            "seqer": seqer_acc
+            "seqer": seqer_acc,
         }
         logs = pd.DataFrame.from_dict(logs)
         logs.to_csv(log_path, index=False)
