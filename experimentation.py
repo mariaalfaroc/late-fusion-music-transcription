ff601e484f7774edd6dabcdd34911c75044795ac
mariaalfaroc <mac77@alu.ua.es>
2024-01-19 17:44:38 +0100

    clean k-fold train and test experiment

--- experimentation.py
+++ experimentation.py
@@ -1,104 +1,88 @@
-# -*- coding: utf-8 -*-
-
-import os, gc, shutil
+import os
+import gc
 
 import pandas as pd
-import numpy as np
 from tensorflow import keras
 
-import config
-from data_processing import (
+from my_utils.kaldi_preprocessing import *
+from networks.models import build_models
+from networks.train import train_and_test_model
+from networks.test import evaluate_model
+from scenarios.folds_creation import (
     get_folds_filenames,
-    get_datafolds_filenames,
-    get_fold_vocabularies,
-    save_w2i_dictionary,
-    load_dictionaries,
-    train_data_generator,
+    get_datafold_filenames,
+    check_and_retrive_vocabulary,
 )
-from models import build_models
-from evaluation import evaluate_model
-from kaldi_preprocessing import *
 
 
-# Utility function for performing a k-fold cross-validation experiment on a single dataset
-# NOTE: It will raise an error if AMT has not been previously evaluated on ScenarioX and the files has not been copied to the rest of scenarios
-def k_fold_experiment(epochs):
+# Utility function for performing a k-fold cross-validation
+# experiment on a single dataset (SCENARIOS A, B, and D)
+def k_fold_experiment(
+    *,
+    task: str,
+    scenario_name: str,
+    epochs: int = 150,
+    batch_size: int = 16,
+):
     keras.backend.clear_session()
     gc.collect()
 
     # ---------- PRINT EXPERIMENT DETAILS
 
-    print("k-fold cross-validation experiment")
-    print(f"Data used {config.base_dir.stem}")
+    print(f"5-fold cross-validation experiment for scenario {scenario_name}")
+    print(f"\tTask: {task}")
+    print(f"\tEpochs: {epochs}")
+    print(f"\tBatch size: {batch_size}")
 
-    # ---------- DATA COLLECTION
+    # ---------- FOLDS COLLECTION
 
-    train_folds_files = get_folds_filenames("train")
-    val_folds_files = get_folds_filenames("val")
-    test_folds_files = get_folds_filenames("test")
+    folds = get_folds_filenames(scenario_name)
 
-    assert len(train_folds_files) == len(val_folds_files) == len(test_folds_files)
+    # ---------- 5-FOLD EVALUATION
 
-    train_images_fnames, train_labels_fnames = get_datafolds_filenames(
-        train_folds_files
-    )
-    val_images_fnames, val_labels_fnames = get_datafolds_filenames(val_folds_files)
-    test_images_fnames, test_labels_fnames = get_datafolds_filenames(test_folds_files)
-
-    # ---------- K-FOLD EVALUATION
-
-    # Start the k-fold evaluation scheme
-    k = len(train_images_fnames)
-    for i in range(k):
+    for id, (train_fold, val_fold, test_fold) in enumerate(
+        zip(folds["train"], folds["val"], folds["test"])
+    ):
         # With 'clear_session()' called at the beginning,
         # Keras starts with a blank state at each iteration
-        # and memory consumption is constant over time.
+        # and memory consumption is constant over time
         keras.backend.clear_session()
         gc.collect()
 
-        print(f"Fold {i}")
-
-        # Set filepaths outputs
-        output_dir = config.output_dir / config.task / f"Fold{i}"
-        os.makedirs(output_dir, exist_ok=True)
-        pred_model_filepath = output_dir / "best_model.keras"
-        w2i_filepath = output_dir / "w2i.json"
-        log_path = output_dir / "logs.csv"
+        print(f"Fold {id}")
 
         # Get the current fold data
-        train_images, train_labels = train_images_fnames[i], train_labels_fnames[i]
-        val_images, val_labels = val_images_fnames[i], val_labels_fnames[i]
-        test_images, test_labels = test_images_fnames[i], test_labels_fnames[i]
+        train_images, train_labels = get_datafold_filenames(
+            task=task, fold_filename=train_fold
+        )
+        val_images, val_labels = get_datafold_filenames(
+            task=task, fold_filename=val_fold
+        )
+        test_images, test_labels = get_datafold_filenames(
+            task=task, fold_filename=test_fold
+        )
+        print(f"Train size: {len(train_images)}")
+        print(f"Validation size: {len(val_images)}")
+        print(f"Test size: {len(test_images)}")
 
-        assert len(train_images) == len(train_labels)
-        assert len(val_images) == len(val_labels)
-        assert len(test_images) == len(test_labels)
-
-        print(f"Train: {len(train_images)}")
-        print(f"Validation: {len(val_images)}")
-        print(f"Test: {len(test_images)}")
-
-        if config.scenario == "X":
-            # Get and save vocabularies
-            w2i, i2w = get_fold_vocabularies(train_labels)
-            save_w2i_dictionary(w2i, w2i_filepath)
-        else:
-            # Load dictionary from Scenario X (from AMT folder)
-            # To use the same vocabulary across scenarios and tasks
-            w2i_filepath_ScenarioX = str(w2i_filepath).replace(
-                f"Scenario{config.scenario}", "ScenarioX"
-            )
-            w2i_filepath_ScenarioX = str(w2i_filepath).replace(f"{config.task}", "amt")
-            print(f"Using vocabulary from {w2i_filepath_ScenarioX}")
-            w2i, i2w = load_dictionaries(w2i_filepath_ScenarioX)
-            shutil.copy(w2i_filepath_ScenarioX, w2i_filepath)
+        # Check and retrieve vocabulary
+        w2i, i2w, w2i_path = check_and_retrive_vocabulary(
+            fold_id=id, retrieve_w2i_path=True
+        )
 
         # Build the models
-        model, prediction_model = build_models(num_labels=len(w2i))
+        model, prediction_model = build_models(task=task, num_labels=len(w2i))
+
+        # Set filepaths outputs
+        output_dir = f"results/scenario{scenario_name}/fold{id}"
+        os.makedirs(output_dir, exist_ok=True)
+        pred_model_filepath = os.path.join(output_dir, f"best_{task}_model.keras")
+        log_path = os.path.join(output_dir, f"{task}_logs.csv")
 
         # Train, validate, and test models
         # Save logs in CSV file
         test_data = train_and_test_model(
+            task=task,
             data=(
                 train_images,
                 train_labels,
@@ -109,26 +93,25 @@ def k_fold_experiment(epochs):
             ),
             vocabularies=(w2i, i2w),
             epochs=epochs,
+            batch_size=batch_size,
             model=model,
             prediction_model=prediction_model,
             pred_model_filepath=pred_model_filepath,
             log_path=log_path,
         )
 
-        # Kaldi preprocessing
-        kaldi_dir = output_dir / "kaldi"
-        os.makedirs(kaldi_dir, exist_ok=True)
+        # Save to Kaldi format
         # test_data = [y_true_acc, raw_y_pred_acc, y_pred_len_acc]
-        kaldi_fold(
-            fold_filepath=test_folds_files[i], kaldi_dir=kaldi_dir, fold_type="test"
-        )
-        kaldi_vocabulary(w2i_filepath=w2i_filepath, kaldi_dir=kaldi_dir)
-        kaldi_groundtruth(
+        kaldi_dir = os.path.join(output_dir, "kaldi")
+        os.makedirs(kaldi_dir, exist_ok=True)
+        write_kaldi_fold(fold_filepath=test_fold, kaldi_dir=kaldi_dir, fold_type="test")
+        write_kaldi_vocabulary(w2i_filepath=w2i_path, kaldi_dir=kaldi_dir)
+        write_kaldi_groundtruth(
             kaldi_dir=kaldi_dir, id_labels=test_labels, labels=test_data[0]
         )
-        kaldi_confmat(
-            kaldi_dir,
-            fold_type="test",
+        write_kaldi_confmat(
+            kaldi_dir=kaldi_dir,
+            fold_type=f"test-{task}",
             id_preds=test_labels,
             preds=test_data[1],
             preds_len=test_data[2],
@@ -136,10 +119,8 @@ def k_fold_experiment(epochs):
 
         # Clear memory
         del train_images, train_labels, val_images, val_labels, test_images, test_labels
-        del model, prediction_model
         del test_data
-
-    return
+        del model, prediction_model
 
 
 # Utility function for performing a k-fold test partition experiment using previously trained models
