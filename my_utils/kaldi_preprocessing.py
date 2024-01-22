e602cc3e78f7014152261e8758d299a3cc12b5ac
mariaalfaroc <mac77@alu.ua.es>
2024-01-22 18:50:16 +0100

    change exists from pathlib to os

--- my_utils/kaldi_preprocessing.py
+++ my_utils/kaldi_preprocessing.py
@@ -9,7 +9,7 @@ from scenarios.folds_creation import load_dictionaries
 
 def write_kaldi_fold(fold_filepath, kaldi_dir, fold_type):
     dst = os.path.join(kaldi_dir, f"ID_{fold_type}.lst")
-    if not dst.exists():
+    if not os.path.exists(dst):
         # Avoid copying the same file for both OMR and AMT models as it is the same
         shutil.copyfile(src=fold_filepath, dst=dst)
 
@@ -17,7 +17,7 @@ def write_kaldi_fold(fold_filepath, kaldi_dir, fold_type):
 def write_kaldi_vocabulary(w2i_filepath, kaldi_dir):
     w2i, _ = load_dictionaries(w2i_filepath)
     kaldi_w2i_path = os.path.join(kaldi_dir, "chars.lst")
-    if not kaldi_w2i_path.exists():
+    if not os.path.exists(kaldi_w2i_path):
         # Avoid copying the same file for both OMR and AMT models as it is the same
         with open(kaldi_w2i_path, "w") as f:
             for w in w2i.keys():
@@ -26,7 +26,7 @@ def write_kaldi_vocabulary(w2i_filepath, kaldi_dir):
 
 def write_kaldi_groundtruth(kaldi_dir, id_labels, labels):
     gt_path = os.path.join(kaldi_dir, "grnTruth.dat")
-    if not gt_path.exists():
+    if not os.path.exists(gt_path):
         # Avoid copying the same file for both OMR and AMT models as it is the same
         with open(gt_path, "w") as f:
             for id, y_true in zip(id_labels, labels):
